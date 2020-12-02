b84c62a4e0ab9bb39a5ad0604e5dd0ce1661260a
AntonChetveryk <chetverykanton92@gmail.com>
2020-12-02 13:38:18 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.59cc0629c6f4b7fae04dfec86ce92806.js"
+  "./precache-manifest.a0b913bd98334b9524c7fc17e24e859e.js"
 );
 
 self.addEventListener('message', (event) => {
